56a79bf4f8c5f2dcf5b00cf2c7f33ad66830bb9f
Yousefh1 <yhassan5252@gmail.com>
2023-02-24 11:21:19 -0500

    worked on the button being on the left side and also pushing the data from the form

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const popContainer = document.getElementById("popup-form-container");
 const popForm = document.getElementById("popup-form");
 const addButton = document.getElementById("close_btn");
 const cancelButton = document.getElementById("cancel-form");
+const bookList = document.getElementById("book-list");
+
 // popContainer.addEventListener("click", () =>
 // style.display = 'none')
 
@@ -14,3 +16,17 @@ cancelButton.addEventListener("click", () => {
 addButton.addEventListener("click", () => {
   popContainer.style.display = 'block';
 });
+
+const bookTemplate = (title, author, pages) => {
+  const html = `
+  <li class='book-item'> ${title} ${author} ${pages}</li>
+`
+bookList.innerHTML += html
+}
+popForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if(title.value && author.value) {
+    bookTemplate(title.value, author.value, pages.value)
+  }
+})
